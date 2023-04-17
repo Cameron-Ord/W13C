26bc1aedbd820abe2ae0f31d73d759d13abe17e2
NuckenMcFuggets <cameron.ian.ronald.ord@Outlook.com>
2023-04-16 20:21:57 -0600

    adding template stuff

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,46 @@
 <template>
-  <div id="app">
-
+  <div>
+    <header class="Page_Header"><page-header></page-header></header>
+    <main class="Page_Main"><page-main></page-main></main>
+    <footer class="Page_Footer"><page-footer></page-footer></footer>
   </div>
 </template>
 
 <script>
-
+import PageHeader from './components/PageHeader.vue';
+import PageMain from './components/PageMain.vue';
+import PageFooter from './components/PageFooter.vue';
+import SongDisplay from './components/SongDisplay.vue';
 
 export default {
-  name: 'App',
+  name: 'Layout',
   components: {
 
+    PageHeader,
+    PageMain,
+    PageFooter,
+    SongDisplay
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+.Page_Header{
+display: grid;
+grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+min-height: 10vh;
+}
+
+.Page_Main{
+display: grid;
+grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+min-height: 80vh;
+}
+
+.Page_Footer{
+display: grid;
+grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+min-height: 10vh;
 }
 </style>
---
+++ src/components/PageHeader.vue
@@ -0,0 +1,26 @@
+<template>
+    <span class="header_span">
+
+        <h1>LOGO</h1>
+
+        <h1>MENU</h1>
+
+    </span>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+.header_span{
+
+    display: grid;
+
+    grid-template-columns: 40vw 40vw;
+    
+}
+
+</style>
