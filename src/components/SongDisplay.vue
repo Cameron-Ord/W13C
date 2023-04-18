ca6ed50143c3d23e37abed2ea50d39ff682ea6ae
NuckenMcFuggets <cameron.ian.ronald.ord@Outlook.com>
2023-04-17 19:44:32 -0600

    tweaks

--- src/components/SongDisplay.vue
+++ src/components/SongDisplay.vue
@@ -1,31 +1,31 @@
 <template>
-        <div class="parent">
-            <section class="section_main">
-                <article class="main_article">
-                        <div class="main_span">
+    <div class="parent">
+        <section class="section_main">
+            <article class="main_article">
+                <div class="main_span">
 
-                            <div class="pick_song">
-                            <p v-if="!isPlaying">pick a song</p>
-                            <p v-else>now playing</p>
-                            <!--if isPlaying = true, injects 'now playing'-->
-                        </div>
-                            <span v-if="isPlaying" class="selection_span">
-                            
-                                <span>
+                    <div class="pick_song">
+                        <p v-if="!isPlaying">pick a song</p>
+                        <p v-else>now playing</p>
+                        <!--if isPlaying = true, injects 'now playing'-->
+                    </div>
+                    <div v-if="isPlaying" class="selection_span">
+
+                        <span>
                             <!--displaying the selected title using a current variable-->
 
                             <h3 v-if="isPlaying" class="song_title">{{ current[`title`] }}</h3>
-                        
-                            <h3 v-if="isPlaying"  class="song_artist">{{ current[`artist`] }}</h3>
+
+                            <h3 v-if="isPlaying" class="song_artist">{{ current[`artist`] }}</h3>
 
                             <!--binding the image that is selected from the object inside the current variable-->
 
-                            <img v-if="isPlaying"  v-bind:src="current[`image_url`]">
-                        
+                            <img v-if="isPlaying" v-bind:src="current[`image_url`]">
+
                         </span>
-                        </span>
-            
-                        <span class="buttons_span">
+                    </div>
+
+                    <span class="buttons_span">
 
                         <!--Some buttons (prev and next do not work)-->
 
@@ -34,7 +34,7 @@
                         <!--an if statement that will show the play button only if isPlaying is set to false-->
 
                         <!--add event listener set to click to call the play() function-->
-                        
+
                         <button class="play" v-if="!isPlaying" @click="play">PLAY</button>
 
                         <!--if isPlaying === true, it will show a pause button that calls the pause function-->
@@ -44,17 +44,17 @@
                         <button class="next">NEXT</button>
 
                         <!--if isPlaying is false, injects 'pick a song' p tag-->
-            
+
 
                     </span>
 
-                    </div>
+                </div>
 
-                </article>
+            </article>
 
-                <article class="playlist">
+            <article class="playlist">
 
-                    <span class="playlist_span">
+                <span class="playlist_span">
 
                     <h3>The Playlist</h3>
 
@@ -64,194 +64,197 @@
 
                     <!--the :class selects the song title and makes it equal to the current title-->
 
-                    <button class="overflow" v-for="song in songs" :key="song[`title`]" @click="play(song)" :class="(song[`title`] == current[`title`])">{{ song[`title`] }} - {{ song[`artist`] }}  </button>
+                    <button class="overflow" v-for="song in songs" :key="song[`title`]" @click="play(song)"
+                        :class="(song[`title`] == current[`title`])">{{ song[`title`] }} - {{ song[`artist`] }} </button>
 
                 </span>
-               
-              
-
-
-
-
-
-                </article>
-            </section>
-        </div>
+            </article>
+        </section>
+    </div>
 </template>
 
 <script>
-    export default {
+export default {
 
 
 
 
-        data() {
-            return {
+    data() {
+        return {
 
-                //current variable to select and hold the song object//
+            //current variable to select and hold the song object//
 
-                current:{},
+            current: {},
 
-                index: 0,
+            index: 0,
 
-                //setting the isPlaying to false by default so that it only plays when you click the correct buttons//
+            //setting the isPlaying to false by default so that it only plays when you click the correct buttons//
 
-                isPlaying: false,
-          
-                //the array of objects containing the key values//
+            isPlaying: false,
 
-                songs: [{
+            //the array of objects containing the key values//
+
+            songs: [{
 
 
-                    title: `Roller Mobster`,
+                title: `Roller Mobster`,
 
-                    artist: `Carpenter Brut`,
+                artist: `Carpenter Brut`,
 
-                    song_id: 7,
+                song_id: 7,
 
-                    image_url: `https://m.media-amazon.com/images/I/51PHl1VZO3L._AC_SX342_.jpg`,
+                image_url: `https://m.media-amazon.com/images/I/51PHl1VZO3L._AC_SX342_.jpg`,
 
-                    src: require(`../assets/roller-mobster.mp3`)
-
-   
+                src: require(`../assets/roller-mobster.mp3`)
 
 
 
-                },
-                {
-
-                    title: `Diminished to B`,
-
-                    artist: `Necrophagist`,
-
-                    song_id: 5,
-
-                    image_url: `https://upload.wikimedia.org/wikipedia/en/thumb/a/af/Epitaph_cover.jpg/220px-Epitaph_cover.jpg`,
-
-                    src: require(`../assets/dimished-to-b.mp3`)
-         
 
 
-                },
-                {
-                    title: `Confine of Shadows`,
+            },
+            {
 
-                    artist: `Disincarnate`,
+                title: `Diminished to B`,
 
-                    song_id: 8,
+                artist: `Necrophagist`,
 
-                    image_url: `https://upload.wikimedia.org/wikipedia/en/f/f9/Disincarnate.jpg`,
+                song_id: 5,
 
-                    src: require(`../assets/confine-of-shadows.mp3`)
-         
+                image_url: `https://upload.wikimedia.org/wikipedia/en/thumb/a/af/Epitaph_cover.jpg/220px-Epitaph_cover.jpg`,
 
-
-                },
-                {
-
-                    title: `The planet that once used to absorb flesh in order to achieve divinity and immortality(suffocated to the flesh it desired)`,
-
-                    artist: `Demilich`,
-
-                    song_id: 9,
-
-                    image_url: `https://upload.wikimedia.org/wikipedia/en/thumb/1/10/Nespithe.JPG/220px-Nespithe.JPG`,
-
-                    src: require(`../assets/planet.mp3`)
-                    
+                src: require(`../assets/dimished-to-b.mp3`)
 
 
 
-                },
-                {
+            },
+            {
+                title: `Confine of Shadows`,
 
-                    title: `Raining Steel`,
+                artist: `Disincarnate`,
 
-                    artist: `Perturbator`,
+                song_id: 8,
 
-                    song_id: 10,
+                image_url: `https://upload.wikimedia.org/wikipedia/en/f/f9/Disincarnate.jpg`,
 
-                    image_url: `https://i.scdn.co/image/3092c7fb063d40e02cb9c890dad854bd1e34fbfc`,
-
-                    src: require(`../assets/raining-steel.mp3`)
-
-                    
+                src: require(`../assets/confine-of-shadows.mp3`)
 
 
-                },
+
+            },
+            {
+
+                title: `The planet that once used to absorb flesh in order to achieve divinity and immortality(suffocated to the flesh it desired)`,
+
+                artist: `Demilich`,
+
+                song_id: 9,
+
+                image_url: `https://upload.wikimedia.org/wikipedia/en/thumb/1/10/Nespithe.JPG/220px-Nespithe.JPG`,
+
+                src: require(`../assets/planet.mp3`)
+
+
+
+
+            },
+            {
+
+                title: `Raining Steel`,
+
+                artist: `Perturbator`,
+
+                song_id: 10,
+
+                image_url: `https://i.scdn.co/image/3092c7fb063d40e02cb9c890dad854bd1e34fbfc`,
+
+                src: require(`../assets/raining-steel.mp3`)
+
+
+
+
+            },
 
             ],
 
-         player: new Audio()
+            //variable for audio//
+            player: new Audio()
 
-            }
-        },
+        }
+    },
 
-        methods: {
-
-
-            play (song) {
-
-                //if the song.src is not undefined, make current equal to the song object//
-
-                if(song[`src`] !== undefined){
-
-                    this.current = song;                      //setting the isPlaying variable to true on click//
-
-                    this.player.src = this.current.src;
-          
-                    this.isPlaying = false;
+    methods: {
 
 
+        play(song) {
 
-                }
+            //if the song.src is not undefined, make current equal to the song object//
 
-                this.player.play();
-                this.isPlaying = true;
-      
+            if (song[`src`] !== undefined) {
 
-          
-            },
+                this.current = song;
 
-            //methods containing the respective functions//
+                //making the player.src = current.src after mount inside the function//
 
-            pause (){
+                this.player.src = this.current.src;
 
+                //stating isPlaying as false inside the if statement to prevent double playback// //not sure if my 'idea' logic is correct on this//
 
-                //setting the isPlaying variable to false on click//
-
-                this.player.pause();
                 this.isPlaying = false;
 
-            },
-
-
-            next(){
-
-                
-
-            },
-
-            prev (){
 
 
             }
-    
+
+            this.player.play();
+
+            //setting the isPlaying variable to true on click//
+
+            this.isPlaying = true;
+
+
+
+        },
+
+        //methods containing the respective functions//
+
+        pause() {
+
+
+            //setting the isPlaying variable to false on click//
+
+            this.player.pause();
+            this.isPlaying = false;
 
         },
 
 
-        created() {
+        next() {
 
-            //defining current before mount, this gives the index for the case of using next/prev buttons//
 
-            this.current = this.songs[this.index];
 
-            this.player.src = this.current.src;
+        },
+
+        prev() {
 
 
         }
+
+
+    },
+
+
+    created() {
+
+        //defining current before mount, this gives the index for the case of using next/prev buttons//
+
+        this.current = this.songs[this.index];
+
+        //making the player.src equal to the current.src//
+        this.player.src = this.current.src;
+
+
     }
+}
 </script>
 
 <style scoped>
